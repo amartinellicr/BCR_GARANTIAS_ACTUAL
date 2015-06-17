b07acdb1d0f5bc1cb37926822e6edb460a5b26f3
amartinellicr <arnoldo.martinelli@gmail.com>
2015-06-17 13:14:13 -0600

    Revert "Primera Versión"
    This reverts commit b4e70befcb680c4cc89bbe815db233667efb3024.

--- FUENTES/BASE DE DATOS/5. PERMISOS/03.FUNCIONES.sql
+++
@@ -1,7 +0,0 @@
-GRANT EXECUTE ON dbo.ufn_ConvertirCodigoGarantia TO RAP_Garantias
-GRANT EXECUTE ON dbo.ufn_EsNumero TO RAP_Garantias
-GRANT EXECUTE ON dbo.ufn_ObtenerTipoCambioSICC TO RAP_Garantias
-GRANT EXECUTE ON dbo.ufn_RedondearValor_FV TO RAP_Garantias
-GRANT EXECUTE ON dbo.ufn_ObtenersiCorrioServicio TO RAP_Garantias
-GRANT SELECT ON dbo.Obtener_Fechas_Semestrales_FT TO RAP_Garantias
-GRANT SELECT ON dbo.Obtener_Operaciones_Comunes_FT TO RAP_Garantias
